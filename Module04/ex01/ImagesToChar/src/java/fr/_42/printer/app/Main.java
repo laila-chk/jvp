f64656393be619298cdbcd78b59388f77ab5a626
laila-chk <chokri.laila.ch@gmail.com>
2026-08-19 22:43:38 +0100

    ex01

---
+++ Module04/ex01/ImagesToChar/src/java/fr/_42/printer/app/Main.java
@@ -0,0 +1,15 @@
+package fr._42.printer.app;
+
+import fr._42.printer.logic.ReadAndPrint;
+
+public class Main {
+  public static void main(String[] args) {
+    if (args.length != 2){
+      System.err.println("Error, Wrong arguments!\nUsage example: java -jar images-to-chars-printer.jar . 0 \n"
+        + "for '.' replacing white pixels and '0' replacing black ones");
+      System.exit(1);
+    }
+    ReadAndPrint readAndPrint = new ReadAndPrint(args);
+    readAndPrint.process();
+  }
+}
---
+++ Module04/ex01/ImagesToChar/src/java/fr/_42/printer/logic/ReadAndPrint.java
@@ -0,0 +1,55 @@
+package fr._42.printer.logic;
+
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+import javax.imageio.ImageIO;
+
+
+public class ReadAndPrint {
+  private String[] args;
+  private String whitePixel;
+  private String blackPixel;
+
+  public ReadAndPrint(String[] args){
+    this.args = args;
+    this.whitePixel = args[0];
+    this.blackPixel= args[1];
+  }
+
+  public void process() {
+    try {
+      String imgPath = System.getProperty("user.dir");
+      while (!imgPath.isEmpty() && !imgPath.endsWith("ImagesToChar"))
+        imgPath = imgPath.substring(0, imgPath.lastIndexOf("/"));
+      imgPath += "/target/resources/image.bmp";
+
+      File imgFile = new File(imgPath);
+      BufferedImage img = ImageIO.read(imgFile);
+
+      if (img != null) {
+        int maxX, maxY;
+        maxX = img.getHeight();
+        maxY = img.getWidth();
+        for (int i = 0; i < maxX; i++){
+          for (int j = 0; j < maxY; j++){
+            if (img.getRGB(j, i) == -1)
+              System.out.print(whitePixel);
+            else
+              System.out.print(blackPixel);
+          }
+          System.out.println("");
+        }
+      }else {
+        System.err.println("Failed to read BMP image!");
+        System.exit(1);
+      }
+
+    } catch (Exception e) {
+        System.err.println("Error! " + e.getMessage());
+        System.exit(1);
+    }
+  }
+}
